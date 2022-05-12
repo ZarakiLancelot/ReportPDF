037f6e79d61591043375a8ca6ed63f6ae04ef760
Einsen Vásquez <33559353+ZarakiLancelot@users.noreply.github.com>
2022-05-12 02:55:56 -0600

    Agregar archivos de proyecto.

---
+++ ReportPDF/Controllers/PDFController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReportPDF.Controllers
+{
+    [ApiController]
+    public class PDFController : ControllerBase
+    {
+        [HttpPost]
+        [Route("api/generarPDF")]
+        public IActionResult Post()
+        {
+            return Ok("PDF Generado!");
+        }
+
+        [HttpGet]
+        [Route("api/report/{id}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok($"PDF #{id}");
+        }
+    }
+}
